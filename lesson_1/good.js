a77247fbaae4c8daa114731a7519b756c3f46396
kuznetsov_a_g <skuznetsov92@mail.ru>
2021-10-16 10:10:31 +0300

    универсальность встаки картинки

--- lesson_1/good.js
+++ lesson_1/good.js
@@ -1,13 +1,13 @@
 const goods = [
-    {titele: 'Shirt', price: 150},
-    {titele: 'Socks', price: 50},
-    {titele: 'Jacket', price: 350},
-    {titele: 'Shoes', price: 250},
+    {titele: 'Shirt', price: 150, img: '12.jpg', },
+    {titele: 'Socks', price: 50, img: '12.jpg',},
+    {titele: 'Jacket', price: 350, img: '12.jpg',},
+    {titele: 'Shoes', price: 250, img: '12.jpg',},
 
 ];
-const renderGoodsItem = (titele, price) => `<div class="goods-item"><h3>${titele}</h3> <img src="12.jpg" alt="photo"><p>${price} $</p> <hr></div>`;
+const renderGoodsItem = (titele, price, img) => `<div class="goods-item"><h3>${titele}</h3> <img src=${img} alt="photo"><p>${price} $</p> <hr></div>`;
 const renderGoodsList = (list = goods)=> {
-    let goodsList = list.map((item) => renderGoodsItem(item.titele, item.price)).join('');
+    let goodsList = list.map((item) => renderGoodsItem(item.titele, item.price, item.img)).join('');
     document.querySelector('.good-list').innerHTML = goodsList;
     
 };
